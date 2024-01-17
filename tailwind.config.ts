b8e2c45d0c851c1ff45ae3a9c598995b935aefd3
alvesxdani <dani.alvesm22@gmail.com>
2024-01-17 20:40:38 -0300

    :wrench: chore: add dark mode config e cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        blackbg: '#333',
+        primaryAccent: '#19C819'
+      }
     },
   },
   plugins: [],
