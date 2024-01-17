3452efa0238709ea13e53f8a7f1a40e8c3eceb18
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-17 13:29:34 -0300

    :wrench: chore: modificando as cores no theme do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,14 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        primary: "var(--color-bg-primary)",
       },
+      textColor: {
+        primary: "var(--color-text-primary)"
+      },
+      colors: {
+        accent: 'var(--color-accent)'
+      }
     },
   },
   plugins: [],
