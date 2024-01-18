92eab646b1f4f5d22ea39b645d6b88a5337cbcd2
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-18 14:25:55 -0300

    :lipstick: adicionando fonte Red Hat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { RedHat } from '@/Fonts/fonts'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-        <body className={`dark:bg-dark dark:text-white bg-white text-dark ${RedHat.variable}`}>{children}</body>
+        <body className={`dark:bg-dark dark:text-white bg-white text-dark ${RedHat.className}`}>{children}</body>
     </html>
   )
 }
