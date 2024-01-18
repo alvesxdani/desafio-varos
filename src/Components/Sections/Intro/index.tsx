1fc4f57441b0be009c0d5d3e26319b8e68add955
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-18 17:23:50 -0300

    :construction: desenvolvendo o slide infinito

--- src/Components/Sections/Intro/index.tsx
+++ src/Components/Sections/Intro/index.tsx
@@ -1,11 +1,12 @@
 import Button from "@/Components/Button";
+import Image from "next/image";
 import React from "react";
 
 const Intro = () => {
   return (
     <section
       id="intro"
-      className="w-[100%] px-4 mt-[160px] flex flex-col items-center gap-[72px]"
+      className="w-[100%] px-4 mt-[160px] flex flex-col items-center gap-[72px] mb-[164px]"
     >
       <div className="flex flex-col items-center justify-center gap-8">
         <div className="flex flex-col gap-6">
@@ -23,8 +24,54 @@ const Intro = () => {
       </div>
 
       <div className="rounded-3xl p-px bg-gradient-to-r from-[#4D5358] to-transparent w-[100%]">
-        <div className="dark:bg-dark bg-white p-10 rounded-3xl m-[1px] text-xl">
-          visto em
+        <div className="dark:bg-dark bg-white p-10 rounded-3xl m-[1px] text-xl flex gap-10">
+          <span className="whitespace-nowrap">visto em</span>
+          <div className="bg-gradient-to-r from-transparent dark:to-dark to-white flex w-100 gap-8 animaInfinite border">
+            <Image
+              src="./Intro/Group.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+            <Image
+              src="./Intro/Group-1.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+            <Image
+              src="./Intro/Group-2.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+
+            <Image
+              src="./Intro/Vector.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+
+            <Image
+              src="./Intro/Group-3.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+            <Image
+              src="./Intro/Group-4.svg"
+              width={25}
+              height={25}
+              alt="Logo"
+              priority={true}
+            />
+          </div>
         </div>
       </div>
     </section>
