325862ab35d597737009a196da04d3352e88a52a
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-18 15:27:12 -0300

    :lipstick: Estilizando a intro da página

---
+++ src/Components/Intro/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="w-[100%] px-4 mt-[160px]">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-center font-semibold text-5xl leading-[50px]">
+          Investir de forma mais inteligente passa por aqui.
+        </h1>
+        <p className="text-center text-lg">
+        Fazemos de tudo para que você possa conquistar seus sonhos da melhor forma possível.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
