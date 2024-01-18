0b2dd00ef19a878a840cc24d466fc1c5657e17a8
alvesxdani <dani.alvesm22@gmail.com>
2024-01-18 20:46:58 -0300

    :lipstick: ajustando o slider em loop

--- src/Components/Sections/Intro/index.tsx
+++ src/Components/Sections/Intro/index.tsx
@@ -26,13 +26,15 @@ const Intro = () => {
       <div className="rounded-3xl p-px bg-gradient-to-r from-[#4D5358] to-transparent w-[100%]">
         <div className="dark:bg-dark bg-white p-10 rounded-3xl m-[1px] text-xl flex gap-10">
           <span className="whitespace-nowrap">visto em</span>
-          <div className="bg-gradient-to-r from-transparent dark:to-dark to-white flex w-100 gap-8 animaInfinite border">
+          <div className="overflow-hidden">
+          <div className="flex w-100 gap-8 animaInfinite w-[180%]">
             <Image
               src="./Intro/Group.svg"
               width={25}
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
             <Image
               src="./Intro/Group-1.svg"
@@ -40,6 +42,7 @@ const Intro = () => {
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
             <Image
               src="./Intro/Group-2.svg"
@@ -47,6 +50,7 @@ const Intro = () => {
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
 
             <Image
@@ -55,6 +59,7 @@ const Intro = () => {
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
 
             <Image
@@ -63,6 +68,7 @@ const Intro = () => {
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
             <Image
               src="./Intro/Group-4.svg"
@@ -70,7 +76,9 @@ const Intro = () => {
               height={25}
               alt="Logo"
               priority={true}
+              className="brightness-0 dark:brightness-100"
             />
+          </div>
           </div>
         </div>
       </div>
