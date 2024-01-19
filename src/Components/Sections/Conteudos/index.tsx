1f3e742f8ba96daa53e1253bfbdc9d2c2d4c77f7
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-19 15:14:34 -0300

    :recycle: refactor: menu de conteúdos

--- src/Components/Sections/Conteudos/index.tsx
+++ src/Components/Sections/Conteudos/index.tsx
@@ -1,14 +1,26 @@
 "use client";
 import Button from "@/Components/Button";
 import Image from "next/image";
-import React, { MouseEventHandler, useState } from "react";
+import React, { Key, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { IoMdArrowDropright } from "react-icons/io";
 
 interface IOptions {
-  id: number;
-  label: string;
-  icon: string;
+  carteiras: {
+    id: number
+    label: string
+    icon: string
+  }[]
+  cursos: {
+    id: number
+    label: string
+    icon: string
+  }[]
+  consultoria: {
+    id: number
+    label: string
+    icon: string
+  }[]
 }
 
 const Conteudos = () => {
@@ -18,15 +30,30 @@ const Conteudos = () => {
     setSelectedTab(tabId);
   };
 
-  const options: IOptions[] = [
-    { id: 1, label: "Carteira Seleção", icon: "/Conteudos/selecao-icon.png" },
-    { id: 2, label: "Carteira FATOR", icon: "/Conteudos/FATOR.png" },
-    { id: 3, label: "Carteira Dividendos", icon: "/Conteudos/Dividendos.png" },
-    { id: 4, label: "Carteira Seleção", icon: "/Conteudos/selecao-icon.png" },
-  ];
+  const options: IOptions = {
+    carteiras: [
+      { id: 1, label: "Carteira Seleção", icon: "/Conteudos/carteiras/selecao-icon.png" },
+      { id: 2, label: "Carteira FATOR", icon: "/Conteudos/carteiras/FATOR.png" },
+      { id: 3, label: "Carteira Dividendos", icon: "/Conteudos/carteiras/Dividendos.png" },
+      { id: 4, label: "Carteira Essencial", icon: "/Conteudos/carteiras/Essencial.png" },
+      { id: 5, label: "Carteira de Flls", icon: "/Conteudos/carteiras/Flls.png" },
+      { id: 6, label: "Carteira Small Caps", icon: "/Conteudos/carteiras/Flls.png" },
+    ],
+    cursos: [
+      { id: 1, label: "Valuation 2.0", icon: "/Conteudos/cursos/valuation.png" },
+      { id: 2, label: "Código.py", icon: "/Conteudos/cursos/CODIGO.png" },
+      { id: 3, label: "Dash.py", icon: "/Conteudos/cursos/Dash.png" },
+      { id: 4, label: "Carteira Essencial", icon: "/Conteudos/cursos/Essencial.png" },
+      { id: 5, label: "Cursos Reels", icon: "/Conteudos/cursos/minicurso.png" },
+      { id: 6, label: "Enciclopédia do Fll", icon: "/Conteudos/cursos/Enciclopedia.png" },
+    ],
+    consultoria: [
+      { id: 1, label: "Carteira Seleção", icon: "/Conteudos/carteiras/selecao-icon.png" },
+    ],
+  };
   return (
-    <section id="conteudos" className="flex flex-col p-2 gap-10">
-      <div className="flex flex-col gap-4">
+    <section id="conteudos" className="flex flex-col p-4 gap-10">
+      <div className="flex flex-col gap-4 md:w-[90%] lg:w-[70%] text-start">
         <h2 className="text-2xl md:text-[38px] text-center md:text-start leading-[120%] font-bold md:max-w-[60%]">
           Simplifique seus investimentos e alcance seus objetivos
         </h2>
@@ -36,9 +63,9 @@ const Conteudos = () => {
         </p>
       </div>
 
-      <div className="flex flex-col w-[100%] md:flex-row gap-8">
+      <div className="flex flex-col w-[100%] md:flex-row gap-8 lg:gap-[100px] md:justify-center">
         <div className="flex flex-col gap-4">
-          <Button bg="grey" format="rounded" icon={<IoMdArrowDropdown />}>
+          {/* <Button bg="grey" format="rounded" icon={<IoMdArrowDropdown />}>
             Carteiras
           </Button>
           <ul className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white">
@@ -46,7 +73,7 @@ const Conteudos = () => {
               return (
                 <li
                   key={id}
-                  className={`p-4 flex gap-4 rounded-[32px] justify-center ${
+                  className={`p-4 flex gap-4 rounded-[32px] justify-center text-sm ${
                     selectedTab === id ? "bg-[#222729] bg-opacity-50" : ""
                   }`}
                   onClick={() => handleTabClick(id)}
@@ -57,9 +84,9 @@ const Conteudos = () => {
                     height={22}
                     alt="Logo"
                     priority={true}
-                    className="w-[23px]"
+                    className="h-[23px]"
                   />
-                  <span>{label}</span>
+                  <span className="whitespace-nowrap">{label}</span>
                 </li>
               );
             })}
@@ -69,11 +96,11 @@ const Conteudos = () => {
           </Button>
           <Button bg="dark" format="rounded" icon={<IoMdArrowDropright />}>
             Consultoria
-          </Button>
+          </Button> */}
         </div>
-        <div className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white">
+        <div className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white md:w-[560px] lg:w-[662px] relative overflow-hidden">
           <div className="flex gap-4 p-4">
-            {selectedTab !== null && (
+            {/* {selectedTab !== null && (
               <Image
                 src={
                   options.find(({ id }) => id === selectedTab)?.icon ||
@@ -88,11 +115,27 @@ const Conteudos = () => {
             )}
             {selectedTab !== null
               ? options.find(({ id }) => id === selectedTab)?.label
-              : null}
+              : null} */}
           </div>
-          <div className="flex">
-            <h3 className="text-xl font-semibold">A combinação perfeita pra você aproveitar a virada da Bolsa</h3>
-            <p>untamos o que há de melhor nas Carteiras VAROS: uma estratégia completa para você ter resultados exponenciais.</p>
+          <div className="flex flex-col gap-4 md:w-[70%] md:min-h-[400px]">
+            <h3 className="text-2xl font-semibold leading-6">
+              A combinação perfeita pra você aproveitar a virada da Bolsa
+            </h3>
+            <p>
+              Juntamos o que há de melhor nas Carteiras VAROS: uma estratégia
+              completa para você ter resultados exponenciais.
+            </p>
+          </div>
+          <div className="rounded-xl md:absolute md:bottom-0 translate-x-6">
+            <Image
+              src="/Conteudos/grafico.png"
+              width={2000}
+              height={2000}
+              alt="Logo"
+              priority={true}
+              className="h-[292px] box-border relative object-cover 
+              md:object-contain md:w-[621px] md:h-auto"
+            />
           </div>
         </div>
       </div>
