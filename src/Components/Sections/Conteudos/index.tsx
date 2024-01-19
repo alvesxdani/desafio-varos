b74276235285e45454043ca6513ff3f838c43ce2
alvesxdani <dani.alvesm22@gmail.com>
2024-01-19 00:18:43 -0300

    :construction: construindo a section de conteudos

--- src/Components/Sections/Conteudos/index.tsx
+++ src/Components/Sections/Conteudos/index.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Conteudos = () => {
   return (
-    <div>Conteudos</div>
+    <section id="conteudos" className="flex flex-col p-2">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-2xl md:text-[38px] text-center md:text-start leading-[120%] font-bold md:max-w-[60%]">
+          Simplifique seus investimentos e alcance seus objetivos
+        </h2>
+        <p className='text-lg text-center md:text-start'>
+        Conteúdos preparados para trazer mais segurança, independente do seu nível.
+        </p>
+      </div>
+
+      <div className='flex flex-col w-[100%] md:flex-row'>
+        <div className='flex flex-col gap-4'>
+          carteiras
+        </div>
+        <div>
+          imagem
+        </div>
+      </div>
+    </section>
   )
 }
 
