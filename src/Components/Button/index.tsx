f1986782964ff4acf48ad181e88c7d4f8a269ff6
alvesxdani <dani.alvesm22@gmail.com>
2024-01-19 01:24:26 -0300

    :construction: construindo a section de conteudos

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -2,7 +2,7 @@ import React, { ButtonHTMLAttributes } from "react";
 
 type ButtonT = ButtonHTMLAttributes<HTMLButtonElement> & {
   format: "square" | "rounded";
-  bg: "green" | "grey"
+  bg: "green" | "grey" | "navy"
   icon?: JSX.Element
 };
 
@@ -10,7 +10,9 @@ const Button = ({ format, bg, children, icon }: ButtonT): JSX.Element => {
   return (
     <button className={`
         ${format === "rounded" ? 'rounded-[48px]' : 'rounded-[5px]'}
-        ${bg === 'green' ? 'bg-primaryAccent bg-opacity-40 text-primaryAccent' : 'bg-secondAccent text-white'}
+        ${bg === 'green' ? 'bg-primaryAccent bg-opacity-40 text-primaryAccent' : 
+          bg === 'grey' ? 'bg-secondAccent text-white': 'bg-[#222729] text-white'
+       }
         py-4 px-12 font-bold flex items-center justify-center gap-3
         `}>
     <span>{children}</span>
--- src/Components/Sections/Conteudos/index.tsx
+++ src/Components/Sections/Conteudos/index.tsx
@@ -2,6 +2,7 @@ import Button from '@/Components/Button'
 import Image from 'next/image'
 import React from 'react'
 import { IoMdArrowDropdown } from 'react-icons/io'
+import { IoMdArrowDropright } from 'react-icons/io'
 
 const Conteudos = () => {
   return (
@@ -16,60 +17,66 @@ const Conteudos = () => {
         </p>
       </div>
 
-      <div className="flex flex-col w-[100%] md:flex-row">
+      <div className="flex flex-col w-[100%] md:flex-row gap-8">
         <div className="flex flex-col gap-4">
           <Button bg="grey" format="rounded" icon={<IoMdArrowDropdown />}>
             Carteiras
           </Button>
-          <ul className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start">
-            <li className='bg-[#222729] bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center'>
-            <Image
-                  src="/Conteudos/selecao-icon.png"
-                  width={22}
-                  height={22}
-                  alt="Logo"
-                  priority={true}
-                  className='w-[22px]'
-                />
+          <ul className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white">
+            <li className="bg-[#222729] bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center">
+              <Image
+                src="/Conteudos/selecao-icon.png"
+                width={22}
+                height={22}
+                alt="Logo"
+                priority={true}
+                className="w-[22px]"
+              />
               <span>Carteira Seleção</span>
             </li>
-            <li className='bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center'>
-            <Image
-                  src="/Conteudos/FATOR.png"
-                  width={20}
-                  height={20}
-                  alt="Logo"
-                  priority={true}
-                  className='w-[23px]'
-                />
+            <li className="bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center">
+              <Image
+                src="/Conteudos/FATOR.png"
+                width={20}
+                height={20}
+                alt="Logo"
+                priority={true}
+                className="w-[23px]"
+              />
               <span>Carteira FATOR</span>
             </li>
 
-            <li className='bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center'>
-            <Image
-                  src="/Conteudos/Dividendos.png"
-                  width={20}
-                  height={20}
-                  alt="Logo"
-                  priority={true}
-                  className='w-[23px]'
-                />
+            <li className="bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center">
+              <Image
+                src="/Conteudos/Dividendos.png"
+                width={20}
+                height={20}
+                alt="Logo"
+                priority={true}
+                className="w-[23px]"
+              />
               <span>Carteira Dividendos</span>
             </li>
-            <li className='bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center'>
-            <Image
-                  src="/Conteudos/selecao-icon.png"
-                  width={22}
-                  height={22}
-                  alt="Logo"
-                  priority={true}
-                  className='w-[22px]'
-                />
+            <li className="bg-opacity-50 p-4 flex gap-4 rounded-[32px] justify-center">
+              <Image
+                src="/Conteudos/selecao-icon.png"
+                width={22}
+                height={22}
+                alt="Logo"
+                priority={true}
+                className="w-[22px]"
+              />
               <span>Carteira Seleção</span>
             </li>
           </ul>
+          <Button bg="navy" format="rounded" icon={<IoMdArrowDropright />}>
+            Cursos
+          </Button>
+          <Button bg="navy" format="rounded" icon={<IoMdArrowDropright />}>
+            Consultoria
+          </Button>
         </div>
-        <div>imagem</div>
+        <div className='bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white'>imagem</div>
       </div>
     </section>
   )
