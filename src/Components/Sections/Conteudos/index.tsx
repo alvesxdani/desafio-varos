55bc02450eea5628d8787fa9ab4c977bf6f452fa
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-19 16:03:57 -0300

    :construction: lógica do menu tab

--- src/Components/Sections/Conteudos/index.tsx
+++ src/Components/Sections/Conteudos/index.tsx
@@ -7,20 +7,26 @@ import { IoMdArrowDropright } from "react-icons/io";
 
 interface IOptions {
   carteiras: {
-    id: number
-    label: string
-    icon: string
-  }[]
+    id: number;
+    label: string;
+    icon: string;
+  }[];
   cursos: {
-    id: number
-    label: string
-    icon: string
-  }[]
+    id: number;
+    label: string;
+    icon: string;
+  }[];
   consultoria: {
-    id: number
-    label: string
-    icon: string
-  }[]
+    id: number;
+    label: string;
+    icon: string;
+  }[];
+}
+
+interface ITab {
+  id: number;
+  label: string;
+  content: { id: number; label: string; icon: string }[];
 }
 
 const Conteudos = () => {
@@ -32,25 +38,83 @@ const Conteudos = () => {
 
   const options: IOptions = {
     carteiras: [
-      { id: 1, label: "Carteira Seleção", icon: "/Conteudos/carteiras/selecao-icon.png" },
-      { id: 2, label: "Carteira FATOR", icon: "/Conteudos/carteiras/FATOR.png" },
-      { id: 3, label: "Carteira Dividendos", icon: "/Conteudos/carteiras/Dividendos.png" },
-      { id: 4, label: "Carteira Essencial", icon: "/Conteudos/carteiras/Essencial.png" },
-      { id: 5, label: "Carteira de Flls", icon: "/Conteudos/carteiras/Flls.png" },
-      { id: 6, label: "Carteira Small Caps", icon: "/Conteudos/carteiras/Flls.png" },
+      {
+        id: 1,
+        label: "Carteira Seleção",
+        icon: "/Conteudos/carteiras/selecao-icon.png",
+      },
+      {
+        id: 2,
+        label: "Carteira FATOR",
+        icon: "/Conteudos/carteiras/FATOR.png",
+      },
+      {
+        id: 3,
+        label: "Carteira Dividendos",
+        icon: "/Conteudos/carteiras/Dividendos.png",
+      },
+      {
+        id: 4,
+        label: "Carteira Essencial",
+        icon: "/Conteudos/carteiras/Essencial.png",
+      },
+      {
+        id: 5,
+        label: "Carteira de Flls",
+        icon: "/Conteudos/carteiras/Flls.png",
+      },
+      {
+        id: 6,
+        label: "Carteira Small Caps",
+        icon: "/Conteudos/carteiras/Flls.png",
+      },
     ],
     cursos: [
-      { id: 1, label: "Valuation 2.0", icon: "/Conteudos/cursos/valuation.png" },
+      {
+        id: 1,
+        label: "Valuation 2.0",
+        icon: "/Conteudos/cursos/valuation.png",
+      },
       { id: 2, label: "Código.py", icon: "/Conteudos/cursos/CODIGO.png" },
       { id: 3, label: "Dash.py", icon: "/Conteudos/cursos/Dash.png" },
-      { id: 4, label: "Carteira Essencial", icon: "/Conteudos/cursos/Essencial.png" },
+      {
+        id: 4,
+        label: "Carteira Essencial",
+        icon: "/Conteudos/cursos/Essencial.png",
+      },
       { id: 5, label: "Cursos Reels", icon: "/Conteudos/cursos/minicurso.png" },
-      { id: 6, label: "Enciclopédia do Fll", icon: "/Conteudos/cursos/Enciclopedia.png" },
+      {
+        id: 6,
+        label: "Enciclopédia do Fll",
+        icon: "/Conteudos/cursos/Enciclopedia.png",
+      },
     ],
     consultoria: [
-      { id: 1, label: "Carteira Seleção", icon: "/Conteudos/carteiras/selecao-icon.png" },
+      {
+        id: 1,
+        label: "Consultoria VAROS",
+        icon: "/Conteudos/consultoria/VZA.png",
+      },
     ],
   };
+
+  const Tabs: ITab[] = [
+    {
+      id: 1,
+      label: "Carteiras",
+      content: options.carteiras,
+    },
+    {
+      id: 2,
+      label: "Cursos",
+      content: options.cursos,
+    },
+    {
+      id: 3,
+      label: "Cursos",
+      content: options.consultoria,
+    },
+  ];
   return (
     <section id="conteudos" className="flex flex-col p-4 gap-10">
       <div className="flex flex-col gap-4 md:w-[90%] lg:w-[70%] text-start">
@@ -65,6 +129,38 @@ const Conteudos = () => {
 
       <div className="flex flex-col w-[100%] md:flex-row gap-8 lg:gap-[100px] md:justify-center">
         <div className="flex flex-col gap-4">
+          {Tabs.map(({ id, label, content }) => {
+            return (
+              <>
+                <Button bg="grey" format="rounded" icon={<IoMdArrowDropdown />}>
+                  {label}
+                </Button>
+                <ul className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white">
+                  {content.map(({ id, label, icon }) => {
+                    return (
+                      <li
+                  key={id}
+                  className={`p-4 flex gap-4 rounded-[32px] justify-center text-sm ${
+                    selectedTab === id ? "bg-[#222729] bg-opacity-50" : ""
+                  }`}
+                  onClick={() => handleTabClick(id)}
+                >
+                  <Image
+                    src={icon}
+                    width={22}
+                    height={22}
+                    alt="Logo"
+                    priority={true}
+                    className="h-[23px]"
+                  />
+                  <span className="whitespace-nowrap">{label}</span>
+                </li>
+                    )
+                  })}
+                </ul>
+              </>
+            );
+          })}
           {/* <Button bg="grey" format="rounded" icon={<IoMdArrowDropdown />}>
             Carteiras
           </Button>
