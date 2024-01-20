4ed805e35f81269d2f419db13339d46d33a9e5b0
alvesxdani <dani.alvesm22@gmail.com>
2024-01-20 14:31:54 -0300

    :sparkles: finalização da section de conteudos

--- src/Components/Sections/Conteudos/index.tsx
+++ src/Components/Sections/Conteudos/index.tsx
@@ -1,124 +1,104 @@
-"use client";
-import Button from "@/Components/Button";
-import Image from "next/image";
-import React, { Key, MouseEventHandler, useState } from "react";
-import { IoMdArrowDropdown } from "react-icons/io";
-import { IoMdArrowDropright } from "react-icons/io";
+'use client'
+import Button from '@/Components/Button'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { IoMdArrowDropdown } from 'react-icons/io'
+import { IoMdArrowDropright } from 'react-icons/io'
 
 interface IOptions {
-  carteiras: {
-    id: number;
-    label: string;
-    icon: string;
-  }[];
-  cursos: {
-    id: number;
-    label: string;
-    icon: string;
-  }[];
-  consultoria: {
-    id: number;
-    label: string;
-    icon: string;
-  }[];
-}
-
-interface ITab {
-  id: number;
-  label: string;
-  content: { id: number; label: string; icon: string }[];
+  title: string
+  children: {
+    id: number
+    label: string
+    icon: string
+  }[]
 }
 
 const Conteudos = () => {
-  const [selectedTitle, setSelectedTitle] = useState<number | null>(1);
-  const [activeTab, setActiveTab] = useState<Record<number, number>>({});
+  const [selectedTitle, setSelectedTitle] = useState<number>(1)
+  const [openIndex, setOpenIndex] = useState<number | null>(0);
 
-  const handleActiveTab = (tabId: number) => {
-    setActiveTab((prevActiveTab) => ({
-      ...prevActiveTab,
-      [tabId]: prevActiveTab[tabId] ? 0 : 1, // Alternar entre 0 e 1
-    }));
+  // Função para manipular a abertura/fechamento do accordion
+  const toggleAccordion = (index: number) => {
+    setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
+    console.log(index)
   };
 
-  const options: IOptions = {
-    carteiras: [
-      {
-        id: 1,
-        label: "Carteira Seleção",
-        icon: "/Conteudos/carteiras/selecao-icon.png",
-      },
-      {
-        id: 2,
-        label: "Carteira FATOR",
-        icon: "/Conteudos/carteiras/FATOR.png",
-      },
-      {
-        id: 3,
-        label: "Carteira Dividendos",
-        icon: "/Conteudos/carteiras/Dividendos.png",
-      },
-      {
-        id: 4,
-        label: "Carteira Essencial",
-        icon: "/Conteudos/carteiras/Essencial.png",
-      },
-      {
-        id: 5,
-        label: "Carteira de Flls",
-        icon: "/Conteudos/carteiras/Flls.png",
-      },
-      {
-        id: 6,
-        label: "Carteira Small Caps",
-        icon: "/Conteudos/carteiras/Flls.png",
-      },
-    ],
-    cursos: [
-      {
-        id: 1,
-        label: "Valuation 2.0",
-        icon: "/Conteudos/cursos/valuation.png",
-      },
-      { id: 2, label: "Código.py", icon: "/Conteudos/cursos/CODIGO.png" },
-      { id: 3, label: "Dash.py", icon: "/Conteudos/cursos/Dash.png" },
-      {
-        id: 4,
-        label: "Carteira Essencial",
-        icon: "/Conteudos/cursos/Essencial.png",
-      },
-      { id: 5, label: "Cursos Reels", icon: "/Conteudos/cursos/minicurso.png" },
-      {
-        id: 6,
-        label: "Enciclopédia do Fll",
-        icon: "/Conteudos/cursos/Enciclopedia.png",
-      },
-    ],
-    consultoria: [
-      {
-        id: 1,
-        label: "Consultoria VAROS",
-        icon: "/Conteudos/consultoria/VZA.png",
-      },
-    ],
-  };
 
-  const Tabs: ITab[] = [
+  const options: IOptions[] = [
     {
-      id: 1,
-      label: "Carteiras",
-      content: options.carteiras,
+      title: 'Carteiras',
+      children: [
+        {
+          id: 1,
+          label: 'Carteira Seleção',
+          icon: '/Conteudos/carteiras/selecao-icon.png',
+        },
+        {
+          id: 2,
+          label: 'Carteira FATOR',
+          icon: '/Conteudos/carteiras/FATOR.png',
+        },
+        {
+          id: 3,
+          label: 'Carteira Dividendos',
+          icon: '/Conteudos/carteiras/Dividendos.png',
+        },
+        {
+          id: 4,
+          label: 'Carteira Essencial',
+          icon: '/Conteudos/carteiras/Essencial.png',
+        },
+        {
+          id: 5,
+          label: 'Carteira de Flls',
+          icon: '/Conteudos/carteiras/Flls.png',
+        },
+        {
+          id: 6,
+          label: 'Carteira Small Caps',
+          icon: '/Conteudos/carteiras/Flls.png',
+        },
+      ],
     },
     {
-      id: 2,
-      label: "Cursos",
-      content: options.cursos,
+      title: 'Cursos',
+      children: [
+        {
+          id: 1,
+          label: 'Valuation 2.0',
+          icon: '/Conteudos/cursos/valuation.png',
+        },
+        { id: 2, label: 'Código.py', icon: '/Conteudos/cursos/CODIGO.png' },
+        { id: 3, label: 'Dash.py', icon: '/Conteudos/cursos/Dash.png' },
+        {
+          id: 4,
+          label: 'Carteira Essencial',
+          icon: '/Conteudos/cursos/Essencial.png',
+        },
+        {
+          id: 5,
+          label: 'Cursos Reels',
+          icon: '/Conteudos/cursos/minicurso.png',
+        },
+        {
+          id: 6,
+          label: 'Enciclopédia do Fll',
+          icon: '/Conteudos/cursos/Enciclopedia.png',
+        },
+      ],
     },
     {
-      id: 3,
-      label: "Consultoria",
-      content: options.consultoria,
+      title: 'Consultoria',
+      children: [
+        {
+          id: 1,
+          label: 'Consultoria VAROS',
+          icon: '/Conteudos/consultoria/VZA.png',
+        },
+      ],
     },
-  ];
+  ]
 
   return (
     <section id="conteudos" className="flex flex-col p-4 gap-10">
@@ -134,28 +114,30 @@ const Conteudos = () => {
 
       <div className="flex flex-col w-[100%] md:flex-row gap-8 lg:gap-[100px] md:justify-center">
         <div className="flex flex-col gap-4">
-          {Tabs.map(({ id: tabId, label, content }) => {
-            const isActive = activeTab[tabId] === 1;
+          {options.map(({ title, children }, index) => {
             return (
-              <div key={tabId} className="flex flex-col gap-4">
+              <div key={title} className="flex flex-col gap-4">
                 <Button
                   bg="grey"
                   format="rounded"
                   icon={<IoMdArrowDropdown />}
-                  onClick={() => handleActiveTab(tabId)}
+                  onClick={() => toggleAccordion(index)}
                 >
-                  {label}
+                  {title}
                 </Button>
-                {isActive && (
-                  <ul className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white" key={`ul-${tabId}`}>
-                    {content.map(({ id: contentId, label, icon }) => {
-                      return (
+                {openIndex  === index && <ul
+                        className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white"
+                      >
+                {children.map(
+                  ({ id, label, icon }) =>
+                   (
                         <li
-                          key={contentId}
-                          className={`p-4 flex gap-4 rounded-[32px] justify-center text-sm ${
-                            selectedTitle === contentId
-                              ? "bg-[#222729] bg-opacity-50"
-                              : ""
+                        key={id}
+                          onClick={() => setSelectedTitle(id)}
+                          className={`p-4 flex gap-4 rounded-[32px] justify-center text-sm cursor-pointer ${
+                            selectedTitle === id
+                              ? 'bg-[#222729] bg-opacity-50'
+                              : ''
                           }`}
                         >
                           <Image
@@ -168,15 +150,15 @@ const Conteudos = () => {
                           />
                           <span className="whitespace-nowrap">{label}</span>
                         </li>
-                      );
-                    })}
-                  </ul>
+                    ),
                 )}
+                </ul>}
               </div>
-            );
+            )
           })}
         </div>
-        <div className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start text-white md:w-[560px] lg:w-[662px] relative overflow-hidden">
+        <div className="bg-[#131516] border-[#222729] border p-8 rounded-[32px] flex flex-col gap-[22px] justify-start items-start 
+          text-white md:w-[560px] lg:w-[662px] relative overflow-hidden">
           <div className="flex gap-4 p-4">
             {/* {selectedTab !== null && (
               <Image
@@ -218,7 +200,7 @@ const Conteudos = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Conteudos;
+export default Conteudos
