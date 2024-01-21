d5bfc79d578bfdf664be38c75584a2f9d7ed46a0
alvesxdani <dani.alvesm22@gmail.com>
2024-01-21 15:09:36 -0300

    :card_file_box: adionando uma variável específica para dados de marketing

---
+++ src/Components/Sections/Marketing/comments.tsx
@@ -0,0 +1,19 @@
+interface IComments {
+  author: string
+  message: string
+}
+
+export const comments: IComments[] = [
+  {
+    author: 'Assinante VAROS',
+    message: `Conteúdos preparados para trazer mais segurança, independente do seu nível. Conteúdos preparados para trazer mais segurança, independente do seu nível.Conteúdos preparados para trazer mais segurança, independente do seu nível.`,
+  },
+  {
+    author: 'Assinante VAROS',
+    message: `Eu acompanho vários gestores de fundo e educadores financeiros. Tu de longe és um dos que mais contribui para minha construção de conhecimento. A forma que colocas coisas que outras pessoas fazem parecer complexas de uma forma super simples e genial. Parabéns! E continue.`,
+  },
+  {
+    author: 'Assinante VAROS',
+    message: `Nunca vi algo tão completo que nem o de vocês. E olha que esses 11 anos que tô no MF já assinei muita casa de análise. Nada chega perto do trabalho seu e da sua equipe.`,
+  },
+]
