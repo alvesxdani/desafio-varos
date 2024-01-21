4eecad4a1df6e5c01febbfe7e7c821d890de2299
alvesxdani <dani.alvesm22@gmail.com>
2024-01-21 18:16:24 -0300

    :lipstick: Adicionando mais um estilo de botão

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,29 +1,45 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from 'react'
 
 type ButtonT = ButtonHTMLAttributes<HTMLButtonElement> & {
-  format: "square" | "rounded";
-  bg: "green" | "grey" | "dark"
+  format: 'square' | 'rounded'
+  bg: 'green' | 'greenLight' | 'grey' | 'dark'
   icon?: JSX.Element
   onClick?: React.MouseEventHandler<HTMLButtonElement>
-};
+  type?: 'submit' | 'button'
+}
 
-const Button = ({ format, bg, children, icon, onClick }: ButtonT): JSX.Element => {
+const Button = ({
+  format,
+  bg,
+  children,
+  icon,
+  onClick,
+  type,
+  className,
+}: ButtonT): JSX.Element => {
   return (
-    <button 
-    onClick={onClick}
-    className={`
-        ${format === "rounded" ? 'rounded-[48px]' : 'rounded-[5px]'}
-        ${bg === 'green' ? 'bg-primaryAccent bg-opacity-40 text-primaryAccent' : 
-          bg === 'grey' ? 'bg-secondAccent text-white': 
-          bg === 'dark' ? 'bg-[#131516] text-white border border-[#222729]' : ''
-       }
-        py-4 px-12 font-bold flex items-center justify-center gap-3
-        `}>
-    <span>{children}</span>
-    {icon}
+    <button
+      onClick={onClick}
+      className={`
+    py-4 px-12 font-bold flex items-center justify-center gap-3
+        ${format === 'rounded' ? 'rounded-[48px]' : 'rounded-[5px]'}
+        ${
+          bg === 'green'
+            ? 'bg-primaryAccent bg-opacity-40 text-primaryAccent'
+            : bg === 'grey'
+            ? 'bg-secondAccent text-white'
+            : bg === 'dark'
+            ? 'bg-[#131516] text-white border border-[#222729]'
+            : bg === 'greenLight'
+            ? 'bg-primaryAccent text-black px-8 text-[18px] font-semibold tracking-wide'
+            : ''
+        }`}
+      type={type}
+    >
+      <span>{children}</span>
+      {icon}
     </button>
   )
-    ;
-};
+}
 
-export default Button;
+export default Button
