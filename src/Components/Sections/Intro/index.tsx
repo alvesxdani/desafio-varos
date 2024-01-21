4497b595585adf3fae9d0e3f30c9c3eb19a928fc
alvesxdani <dani.alvesm22@gmail.com>
2024-01-21 20:29:40 -0300

    :bug: fix: tamanho dos ícones

--- src/Components/Sections/Intro/index.tsx
+++ src/Components/Sections/Intro/index.tsx
@@ -85,49 +85,48 @@ const Intro = () => {
       </div>
 
       <div className="hidden md:flex flex-col gap-20 justify-center">
-
         <div className='after:w-[128px] after:h-[97px] after:bg-[#39FFD9] after:content-[""] after:absolute after:rounded-full after:blur-lg after:opacity-10 after:z-[-99] after:right-0 flex items-center dark:border-[#4D5358] border p-2 gap-4 rounded-2xl z-30 relative dark:bg-dark bg-white mr-20'>
-        <div className='p-3 dark:border-[#4D5358] border text-center relative w-[50px] h-[50px] rounded-lg'>
-          <Image
-                  src="/Intro/FATOR.png"
-                  width={100}
-                  height={100}
-                  alt="Logo"
-                />
+          <div className="p-3 dark:border-[#4D5358] border text-center relative w-[50px] h-[50px] rounded-lg flex justify-center items-center">
+            <Image
+              src="/Intro/FATOR.png"
+              width={100}
+              height={100}
+              alt="Logo"
+              className="w-5"
+            />
           </div>
-        <span className='whitespace-nowrap mr-20'>
-        Carteira FATOR
-        </span>
+          <span className="whitespace-nowrap mr-20">Carteira FATOR</span>
         </div>
 
         <div className='after:w-[128px] after:h-[97px] after:bg-[#D3D6DF] after:content-[""] after:absolute after:rounded-full after:blur-lg after:opacity-10 after:z-[-99] after:left-0 flex items-center dark:border-[#4D5358] border p-2 gap-4 rounded-2xl z-30 relative dark:bg-dark bg-white ml-20'>
-        <div className='p-3 dark:border-[#4D5358] border text-center relative w-[50px] h-[50px] rounded-lg dark:bg-dark bg-white'>
-          <Image
-                  src="/Intro/Dividendos.png"
-                  width={100}
-                  height={100}
-                  alt="Logo"
-                />
+          <div className="p-3 dark:border-[#4D5358] border text-center relative w-[50px] h-[50px] rounded-lg dark:bg-dark bg-white flex justify-center items-center">
+            <Image
+              src="/Intro/Dividendos.png"
+              width={100}
+              height={100}
+              alt="Logo"
+              className="w-5"
+            />
           </div>
-        <span className='whitespace-nowrap mr-20'>
-        Carteira de Dividendos
-        </span>
+          <span className="whitespace-nowrap mr-20">
+            Carteira de Dividendos
+          </span>
         </div>
 
         <div className='after:w-[128px] after:h-[97px] after:bg-[#1CB799] after:content-[""] after:absolute after:rounded-full after:blur-lg after:opacity-10 after:z-[-99] after:right-0 flex items-center dark:border-[#4D5358] border p-2 gap-4 rounded-2xl z-30 relative dark:bg-dark bg-white mr-20'>
-        <div className='p-3 dark:border-[#4D5358] border flex justify-center items-center relative w-[50px] h-[50px] rounded-lg'>
-          <Image
-                  src="/Intro/codigo.png"
-                  width={100}
-                  height={100}
-                  alt="Logo"
-                />
+          <div className="p-3 dark:border-[#4D5358] border flex justify-center items-center relative w-[50px] h-[50px] rounded-lg">
+            <Image
+              src="/Intro/codigo.png"
+              width={100}
+              height={100}
+              alt="Logo"
+              className="w-5"
+            />
           </div>
-        <span className='whitespace-nowrap mr-20'>
-        Código<span className='text-[#00F0C9]'>.py</span>
-        </span>
+          <span className="whitespace-nowrap mr-20">
+            Código<span className="text-[#00F0C9]">.py</span>
+          </span>
         </div>
-
       </div>
     </section>
   )
