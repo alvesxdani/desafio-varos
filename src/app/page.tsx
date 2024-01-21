e6dbe550413cd2b29f85a5005f59afb706611972
alvesxdani <dani.alvesm22@gmail.com>
2024-01-21 18:22:25 -0300

    :lipstick: adicionando o footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Newsletter from '@/Components/Sections/Newsletter'
 import Conteudos from '@/Components/Sections/Conteudos'
 import Intro from '@/Components/Sections/Intro'
 import Marketing from '@/Components/Sections/Marketing'
+import Footer from '@/Components/Footer'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Marketing />
         <Newsletter />
       </main>
+      <Footer />
     </>
   )
 }
